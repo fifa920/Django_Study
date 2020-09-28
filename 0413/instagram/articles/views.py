69acdaada7f7d9e42248ec6ff132b50f139242d0
Daniel_Lee <rodgerslee92@gmail.com>
2020-09-28 17:31:58 +0900

    Add Index Page

--- 0413/instagram/articles/views.py
+++ 0413/instagram/articles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
+from . models import Article
 # Create your views here.
+def index(request):
+    articles = Article.objects.order_by('-pk')
+    context = {
+        'articles' : articles
+    }
+    return render(request, 'articles/index.html', context)
