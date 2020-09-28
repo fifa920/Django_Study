40245c90960b1cda2a779e2414180b31233eb4a4
Daniel_Lee <rodgerslee92@gmail.com>
2020-09-28 17:17:14 +0900

    Article form, url 설정

--- 0413/instagram/articles/admin.py
+++ 0413/instagram/articles/admin.py
@@ -4,4 +4,5 @@ from .models import Article
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
     list_display=['id', 'title', 'created_at', 'updated_at']
+
 admin.site.register(Article, ArticleAdmin)
