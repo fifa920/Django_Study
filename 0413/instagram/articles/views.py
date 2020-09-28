0e94d1c9c6a5c7c8759e6ed42263e181ac0c83bd
Daniel_Lee <rodgerslee92@gmail.com>
2020-09-28 18:21:24 +0900

    Add create page with message framework

--- 0413/instagram/articles/views.py
+++ 0413/instagram/articles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from . models import Article
+from .forms import ArticleForm
+
 # Create your views here.
 def index(request):
     articles = Article.objects.order_by('-pk')
@@ -7,3 +10,18 @@ def index(request):
         'articles' : articles
     }
     return render(request, 'articles/index.html', context)
+
+def create(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            # form.save()
+            return redirect('articles:index')
+        messages.error(request, 'form 다시 확인 ㄱㄱ')
+    else :
+        form = ArticleForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'articles/forms.html', context)
