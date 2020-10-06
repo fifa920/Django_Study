775a76528288978172752569dd8925c1e4849fb8
Daniel_Lee <rodgerslee92@gmail.com>
2020-10-06 18:25:28 +0900

    Accounts 02 | Detail page

--- 0407/django_form/accounts/views.py
+++ 0407/django_form/accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 def signup(request):
@@ -14,4 +16,14 @@ def signup(request):
         'form': form
     }
     return render(request, 'accounts/signup.html', context)
+
+# 개인 프로필 페이지 느낌
+def detail(request, pk):
+    User = get_user_model()
+    user = get_object_or_404(User, pk=pk)
+    context = {
+        'user':user
+    }
+    return render(request, 'accounts/detail.html', context)
+
 
